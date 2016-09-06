7a3e767201a010cd890f3103074173aebd40aaea
mparab <mparab@solarcity.com>
2016-09-06 14:10:54 -0700

    palindrome Alog

---
+++ CheckIfStringPalindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo
+{
+    public class CheckIfStringPalindrome
+    {
+        //aba , aabb 
+        //comapre the first half with other half
+        public static bool CheckPalindrome(string input)
+        {
+            for (int i = 0; i <= input.Length /2; i++)
+            {
+                if (input[i] != input[input.Length -1-i])
+                {
+                    return false;
+                }                  
+            }
+            return true;
+        }
+
+    }
+}
